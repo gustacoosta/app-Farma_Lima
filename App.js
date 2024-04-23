060891b748acc8e00c9c2b80d580be6dabdfb2c5
Gustavo Lima Costa <101073389+gustacoosta@users.noreply.github.com>
2024-04-22 22:07:35 -0300

    Atualizando

--- App.js
+++ App.js
@@ -1,17 +1,73 @@
-import { useFonts, Poppins_500Medium, Poppins_600SemiBold } from "@expo-google-fonts/poppins"
+import React from "react";
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
 import { View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 
 import Produto from "./src/telas/produtos";
 import Mocks from "./src/mocks/produto";
 
-export default function App() {
-  /*
-  //Checa se as fontes já foram carregadas
-  if (!fonteCarregada) {
-    return <View />
-  }
-  */
-
-  return <Produto {...Mocks} />
+function MenuKit() {
+  return <View>
+    <Produto {...Mocks} />
+  </View>
 }
 
+const Tab = createBottomTabNavigator();
+
+function TabsMenu() {
+  return <Tab.Navigator
+    screenOptions={
+      ({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Kit") {
+            iconName = focused
+              ? 'medkit'
+              : 'medkit-outline';
+          } else if (route.name === "Produtos") {
+            iconName = focused
+              ? 'list'
+              : 'list-outline';
+          } else if (route.name === "Lista de Desejos") {
+            iconName = focused
+              ? 'heart'
+              : 'heart-outline';
+          } else if (route.name === "Sobre nós") {
+            iconName = focused
+              ? 'information-circle'
+              : 'information-circle-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+        tabBarActiveTintColor: '#ADFF2F',
+        tabBarInactiveTintColor: 'gray',
+        tabBarHideOnKeyboard: true,
+      })}>
+    <Tab.Screen name='Produtos' component={MenuKit} />
+    <Tab.Screen name='Kit' component={MenuKit} />
+    <Tab.Screen name='Lista de Desejos' component={MenuKit} />
+    <Tab.Screen name='Sobre nós' component={MenuKit} />
+
+  </Tab.Navigator>
+}
+
+export default function App() {
+  let [fonteCarregada] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  //Checa se as fontes já foram carregadas
+  if (!fonteCarregada) {
+    return <View />;
+  }
+
+  return <NavigationContainer>
+    <TabsMenu />
+  </NavigationContainer>
+}
